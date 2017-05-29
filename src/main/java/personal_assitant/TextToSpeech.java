05a54999b38f2169247766c2ce0ce656973c2b89
unknown <matthijs.bergsma@live.nl>
2017-05-30 00:34:59 +0200

    no message

--- src/main/java/personal_assitant/TextToSpeech.java
+++ src/main/java/personal_assitant/TextToSpeech.java
@@ -1,9 +1,14 @@
 package personal_assitant;
 
 public class TextToSpeech {
-
+	static final String NAME = "txt2wav";
+	static final String IN_OPT = "input";
+	static final String OUT_OPT = "output";
 	public TextToSpeech() {
-		// TODO Auto-generated constructor stub
+		test();
 	}
 
+	private void test(){
+		
+	}
 }
